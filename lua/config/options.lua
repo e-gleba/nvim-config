fc01441145832a1fd55606df7504e9d721acd88e
evgeniy.gleba <e_gleba@lesta.group>
2026-02-05 12:06:53 +0300

    use cmake-format by default

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -15,3 +15,7 @@ vim.opt.wrap = false
 vim.g.lazyvim_python_lsp = 'basedpyright'
 -- Set to "ruff_lsp" to use the old LSP implementation version.
 vim.g.lazyvim_python_ruff = 'ruff'
+
+if vim.fn.has('win32') == 1 then
+    LazyVim.terminal.setup('pwsh')
+end
