e762f779df8df6164c4c968ac6178fb1b96fb22c
evgeniy.gleba <e_gleba@lesta.group>
2026-02-05 12:07:05 +0300

    use cmake-format by default

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,10 +1,19 @@
 return {
     {
         'stevearc/conform.nvim',
+        dependencies = { 'mason.nvim' },
         opts = {
             formatters_by_ft = {
-                cmake = { 'cmake_format' }, -- or: { "gersemi" }
+                cmake = { 'cmake_format' },
             },
         },
+    },
+
+    {
+        'mason-org/mason.nvim',
+        opts = function(_, opts)
+            opts.ensure_installed = opts.ensure_installed or {}
+            vim.list_extend(opts.ensure_installed, { 'cmakelang' }) -- provides cmake-format
+        end,
     },
 }
