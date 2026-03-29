9cdedcfc2dc27897980f247dcf881ad9636cd00d
evgeniy.gleba <i@egleba.ru>
2026-03-29 15:14:52 +0300

    upgrade options to use modern lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,30 +1,67 @@
+-- lua/config/options.lua
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.fileformat = 'unix'
-vim.opt.fileformats = { 'unix', 'dos' }
 
-vim.opt.encoding = 'utf-8'
-vim.opt.fileencoding = 'utf-8'
+local o = vim.opt
+local g = vim.g
+local is_win = vim.fn.has('win32') == 1
 
-vim.opt.textwidth = 80
-vim.opt.wrap = false
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.expandtab = true -- табы -> пробелы
+---------------------------------------------------------------------------
+-- encoding (vim.o.encoding defaults to utf-8 in nvim, fileencoding does not)
+---------------------------------------------------------------------------
+o.fileencoding = 'utf-8'
+o.fileformat = 'unix'
+o.fileformats = { 'unix', 'dos' }
 
--- LSP Server to use for Python.
--- Set to "basedpyright" to use basedpyright instead of pyright.
-vim.g.lazyvim_python_lsp = 'basedpyright'
--- Set to "ruff_lsp" to use the old LSP implementation version.
-vim.g.lazyvim_python_ruff = 'ruff'
+---------------------------------------------------------------------------
+-- indentation
+---------------------------------------------------------------------------
+o.textwidth = 80
+o.shiftwidth = 4
+o.tabstop = 4
+o.softtabstop = 4
+o.expandtab = true
+o.wrap = false
 
-if vim.loop.os_uname().sysname == 'Windows_NT' then
-    vim.opt.shell = 'pwsh'
-    vim.opt.shellcmdflag =
-        '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command'
+---------------------------------------------------------------------------
+-- gutter: no sign column, diagnostics via virtual text only
+---------------------------------------------------------------------------
+o.signcolumn = 'no'
+
+vim.diagnostic.config({
+    signs = false,
+    virtual_text = {
+        spacing = 4,
+        prefix = '●',
+        ---@param d vim.Diagnostic
+        format = function(d)
+            return d.message:match('^[^\n]+') -- first line only
+        end,
+    },
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+    float = {
+        border = 'rounded',
+        source = true,
+    },
+})
+
+---------------------------------------------------------------------------
+-- python tooling
+---------------------------------------------------------------------------
+g.lazyvim_python_lsp = 'basedpyright'
+g.lazyvim_python_ruff = 'ruff'
+
+---------------------------------------------------------------------------
+-- windows: pwsh as shell
+---------------------------------------------------------------------------
+if is_win then
+    o.shell = 'pwsh'
+    o.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command'
+    o.shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait'
+    o.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+    o.shellquote = ''
+    o.shellxquote = ''
 end
-
-vim.opt.signcolumn = 'no'
-vim.diagnostic.config({ signs = false })
