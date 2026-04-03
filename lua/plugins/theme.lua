d53ff93fe1e365c58b48f646600e83d6a68ceb89
evgeniy.gleba <e_gleba@lesta.group>
2026-04-03 16:19:15 +0300

    simplify theme settings

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,60 +1,10 @@
+---@module 'lazyvim'
+
 ---@type LazyPluginSpec[]
 return {
     {
         'LazyVim/LazyVim',
         ---@type LazyVimOptions
-        opts = { colorscheme = 'catppuccin' },
-    },
-    {
-        'catppuccin/nvim',
-        name = 'catppuccin',
-        lazy = true,
-        priority = 1000,
-        ---@type CatppuccinOptions
-        opts = {
-            flavour = 'latte',
-            background = { light = 'latte', dark = 'mocha' },
-            styles = {
-                comments = { 'italic' },
-                keywords = { 'bold' },
-                types = { 'bold' },
-                booleans = { 'bold' },
-                conditionals = {},
-                functions = {},
-                strings = {},
-                variables = {},
-                numbers = {},
-                properties = {},
-                operators = {},
-            },
-            integrations = {
-                cmp = true,
-                flash = true,
-                fzf = true,
-                gitsigns = true,
-                grug_far = true,
-                illuminate = true,
-                indent_blankline = { enabled = true },
-                lsp_trouble = true,
-                mason = true,
-                mini = true,
-                navic = { enabled = true, custom_bg = 'lualine' },
-                noice = true,
-                notify = true,
-                snacks = true,
-                treesitter = true,
-                treesitter_context = true,
-                which_key = true,
-                native_lsp = {
-                    enabled = true,
-                    underlines = {
-                        errors = { 'undercurl' },
-                        hints = { 'undercurl' },
-                        warnings = { 'undercurl' },
-                        information = { 'undercurl' },
-                    },
-                },
-            },
-        },
+        opts = { colorscheme = 'catppuccin-latte' },
     },
 }
