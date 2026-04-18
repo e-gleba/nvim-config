70880453848547c02de5795ff8b425229e0e3634
e-gleba <i@egleba.ru>
2026-04-18 17:08:26 +0300

    remove bloat; use defaul clangd, cmake-format opts

--- lua/plugins/clangd.lua
+++ lua/plugins/clangd.lua
@@ -1,50 +1,7 @@
----@type integer
-local nproc = #vim.uv.cpu_info()
-
 ---@type LazyPluginSpec[]
 return {
     {
         'dchinmay2/clangd_extensions.nvim',
         lazy = true,
-        opts = {
-            memory_usage = { border = 'rounded' },
-            symbol_info = { border = 'rounded' },
-        },
-    },
-    {
-        'neovim/nvim-lspconfig',
-        opts = {
-            servers = {
-                clangd = {
-                    cmd = {
-                        'clangd',
-                        '--background-index',
-                        '--clang-tidy',
-                        '--header-insertion=iwyu',
-                        '--completion-style=detailed',
-                        '--function-arg-placeholders',
-                        '--fallback-style=llvm',
-                        '--pch-storage=memory',
-                        '--all-scopes-completion',
-                        '--header-insertion-decorators',
-                        '--enable-config',
-                        '--limit-references=200',
-                        '--limit-results=30',
-                        '-j=' .. tostring(nproc),
-                    },
-                    init_options = {
-                        usePlaceholders = true,
-                        completeUnimported = true,
-                        clangdFileStatus = true,
-                    },
-                },
-            },
-            setup = {
-                ---@return boolean|nil
-                clangd = function()
-                    require('clangd_extensions').setup()
-                end,
-            },
-        },
     },
 }
--- lua/plugins/fmt_cmake.lua
+++ lua/plugins/fmt_cmake.lua
@@ -1,12 +1,3 @@
----@type string[]
-local config_files = {
-    '.cmake-format',
-    '.cmake-format.py',
-    '.cmake-format.yaml',
-    '.cmake-format.json',
-    'CMakeLists.txt',
-}
-
 ---@type LazyPluginSpec[]
 return {
     {
@@ -16,44 +7,6 @@ return {
             formatters_by_ft = {
                 cmake = { 'cmake_format' },
             },
-            ---@type table<string, conform.FormatterConfigOverride>
-            formatters = {
-                cmake_format = {
-                    ---@type string[]
-                    prepend_args = {
-                        '--line-width',
-                        '80',
-                        '--tab-size',
-                        '4',
-                        '--use-tabchars',
-                        'false',
-                        '--max-subgroups-hwrap',
-                        '3',
-                        '--max-pargs-hwrap',
-                        '6',
-                        '--dangle-parens',
-                        'true',
-                    },
-                    ---@param self conform.FormatterConfig
-                    ---@param ctx conform.Context
-                    ---@return string|nil
-                    cwd = function(self, ctx)
-                        return require('conform.util').root_file(config_files)(
-                            self,
-                            ctx
-                        )
-                    end,
-                    require_cwd = false,
-                },
-            },
         },
-    },
-    {
-        'mason-org/mason.nvim',
-        ---@param opts MasonSettings | { ensure_installed?: string[] }
-        opts = function(_, opts)
-            opts.ensure_installed = opts.ensure_installed or {}
-            vim.list_extend(opts.ensure_installed, { 'cmakelang' })
-        end,
     },
 }
