a58c8c723d8daa367cbb754e463bedd4268cd5e7
evgeniy.gleba <i@egleba.ru>
2026-05-09 23:33:00 +0300

    remote.lua: tested linux-linux ssh devbox

---
+++ lua/plugins/remote.lua
@@ -0,0 +1,13 @@
+-- lua/plugins/remote.lua
+return {
+    {
+        'amitds1997/remote-nvim.nvim',
+        version = '*',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'MunifTanjim/nui.nvim',
+            'nvim-telescope/telescope.nvim',
+        },
+        config = true,
+    },
+}
